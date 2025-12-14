a71fe9e12965c66902a2b790c72f54973da6a266
52Blue05 <trinhnam30092005@gmail.com>
2025-12-14 23:00:41 +0700

    #
    Refactor ZDT1 function to include comments for clarity.

--- ZDT1.m
+++ ZDT1.m
@@ -1,13 +1,17 @@
-function f = ZDT1(x)
-
-n = numel(x);
-f1 = x(1);
-
-g = 1 + 9 * sum(x(2:n)) / (n-1);
-
-h = 1 - sqrt(f1/g);
-
-f2 = g * h;
-
-f = [f1 f2];
+function z = ZDT1(x)
+    n = numel(x);
+    
+    % Hàm f1
+    f1 = x(1);
+    
+    % Hàm g: g = 1 + 9 * sum(x(2:end)) / (n - 1)
+    g = 1 + 9 * sum(x(2:end)) / (n - 1);
+    
+    % Hàm h
+    h = 1 - sqrt(f1 / g);
+    
+    % Hàm f2
+    f2 = g * h;
+    
+    z = [f1, f2];
 end
